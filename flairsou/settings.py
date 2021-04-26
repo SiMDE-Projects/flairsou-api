34e143e9157dca96f124282cfe564702ad6373a7
Thibaud Duhautbout <thibaud@duhautbout.ovh>
2021-04-26 16:15:06 +0200

    Ajout de l'app pour l'API

--- flairsou/settings.py
+++ flairsou/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'flairsou_api.apps.FlairsouApiConfig',  # API app
 ]
 
 MIDDLEWARE = [
---
+++ flairsou_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FlairsouApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'flairsou_api'
