8eb8e837f93d0e994cf81ae6a0a7f98d95e8b6f5
Rémy Huet <pro@remyhuet.fr>
2021-04-29 12:01:46 +0200

    Premier modèle : Entité

--- flairsou_api/models.py
+++ flairsou_api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Entity(models.Model):
+    name = models.CharField("Entity name", max_length=64, primary_key=True)
+    use_equity = models.BooleanField("Use Equity Accounts", default=False)
+    parent = models.ForeignKey('self',
+                               on_delete=models.PROTECT,
+                               blank=True,
+                               null=True)
