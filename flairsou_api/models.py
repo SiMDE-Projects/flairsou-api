d75fcc165de817fa52770cec56dc1b7b630c1002
Thibaud Duhautbout <thibaud@duhautbout.ovh>
2021-04-30 11:17:00 +0200

    Ajout de documentation pour le modèle entité et tests de base

--- flairsou_api/models.py
+++ flairsou_api/models.py
@@ -1,9 +1,23 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Entity(models.Model):
+    """
+    Modèle d'entité.
+
+    Attributes
+    ----------
+    name : CharField
+        Champ de texte qui enregistre le nom de l'entité. Clé primaire de la relation.
+
+    use_equity : BooleanField
+        Champ booléen qui indique si l'entité est autorisée à utiliser les comptes de capitaux
+        propres directement ou non.
+
+    parent : ForeignKey
+        Clé étrangère vers une autre instance de Entity, qui représente l'entité parente
+        de l'entité courante
+    """
     name = models.CharField("Entity name", max_length=64, primary_key=True)
     use_equity = models.BooleanField("Use Equity Accounts", default=False)
     parent = models.ForeignKey('self',
--- flairsou_api/tests.py
+++ flairsou_api/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.db.utils import IntegrityError
+from flairsou_api.models import Entity
 
-# Create your tests here.
+
+class EntityTestCase(TestCase):
+    def setUp(self):
+        BDE = Entity.objects.create(name="BDE-UTC")
+        Entity.objects.create(name="PAE-UTC", parent=BDE)
+
+    def test_parent(self):
+        # Vérification de la bonne prise en compte du parent
+        BDE = Entity.objects.get(name="BDE-UTC")
+        PAE = Entity.objects.get(name="PAE-UTC")
+        self.assertEqual(PAE.parent, BDE)
+
+    def test_unique_constraint(self):
+        # Vérification de la contrainte d'unicité
+        self.assertRaises(IntegrityError,
+                          Entity.objects.create,
+                          name="BDE-UTC")
