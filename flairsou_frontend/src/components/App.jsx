f216d536c55baeec94ca41e27d873f9b96eb78f9
Rémy Huet <pro@remyhuet.fr>
2021-05-04 15:34:34 +0200

    Ajout prop-types, passage App en fonction fléchée

--- flairsou_frontend/src/components/App.jsx
+++ flairsou_frontend/src/components/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-function App() {
-  return (
-    <>
-      <h1>Hello world !</h1>
-      <p>Petit bonjour de React / django</p>
-    </>
-  );
-}
+const App = () => {
+  <>
+    <h1>Hello world !</h1>
+    <p>Petit bonjour de React / django</p>
+  </>;
+};
 
 export default App;
