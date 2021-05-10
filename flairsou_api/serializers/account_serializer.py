f048b852d4d42eccf14d73d9ee422f8225867240
Thibaud Duhautbout <thibaud@duhautbout.ovh>
2021-05-10 22:18:56 +0200

    Fix la validation en cas de mise à jour d'un objet

--- flairsou_api/serializers/account_serializer.py
+++ flairsou_api/serializers/account_serializer.py
@@ -18,6 +18,14 @@ class AccountSerializer(serializers.ModelSerializer):
         parent = data['parent']
         book = data['book']
 
+        # si la requête est POST, on fait les tests pour la création d'un nouveau compte, on
+        # va donc vérifier des contraintes d'existence en bdd qui ne doivent pas être vérifiées
+        # pour une mise à jour.
+        if self.context['request'].method == 'POST':
+            create: bool = True
+        else:
+            create: bool = False
+
         # un livre ou un parent mais pas les deux
         if parent is not None and book is not None:
             raise serializers.ValidationError(
@@ -29,16 +37,21 @@ class AccountSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(
                 'Un compte rattaché à un parent doit avoir un type null')
 
-        # vérification d'un autre compte de même nom sous le parent
-        if parent is not None:
-            if parent.account_set.filter(name=name).count() > 0:
-                raise serializers.ValidationError(
-                    'Un compte avec ce nom existe déjà dans le compte parent')
+        if create:
+            # ces points doivent être vérifiés uniquement à la création et pas à la mise à jour
 
-        # vérification d'un autre compte de même nom sous le livre
-        if book is not None:
-            if book.account_set.filter(name=name).count() > 0:
-                raise serializers.ValidationError(
-                    'Un compte avec ce nom existe déjà dans le livre parent')
+            # vérification d'un autre compte de même nom sous le parent
+            if parent is not None:
+                if parent.account_set.filter(name=name).count() > 0:
+                    raise serializers.ValidationError(
+                        'Un compte avec ce nom existe déjà dans le compte parent'
+                    )
+
+            # vérification d'un autre compte de même nom sous le livre
+            if book is not None:
+                if book.account_set.filter(name=name).count() > 0:
+                    raise serializers.ValidationError(
+                        'Un compte avec ce nom existe déjà dans le livre parent'
+                    )
 
         return data
