9d66265a06cab761d6f9e99e53673b995d572930
Thibaud Duhautbout <thibaud@duhautbout.ovh>
2021-05-13 19:50:15 +0200

    Ajout de noms aux urls pour faire du reverse

--- flairsou_api/urls.py
+++ flairsou_api/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 app_name = 'flairsou_api'
 urlpatterns = [
-    path('accounts/', views.AccountList.as_view()),
-    path('accounts/<int:pk>/', views.AccountDetail.as_view()),
+    path('accounts/', views.AccountList.as_view(), name="account-list"),
+    path('accounts/<int:pk>/',
+         views.AccountDetail.as_view(),
+         name="account-detail"),
 ]
