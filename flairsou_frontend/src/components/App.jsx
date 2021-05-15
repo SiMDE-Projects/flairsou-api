0233846f7166ab93709440c7824e09485bf85639
Rémy Huet <pro@remyhuet.fr>
2021-05-15 15:32:14 +0200

    Correct arrow function

--- flairsou_frontend/src/components/App.jsx
+++ flairsou_frontend/src/components/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const App = () => {
+const App = () => (
   <>
     <h1>Hello world !</h1>
     <p>Petit bonjour de React / django</p>
-  </>;
-};
+  </>
+);
 
 export default App;
