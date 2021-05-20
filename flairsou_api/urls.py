98f597035af5fa162620ad11fd3637f9bdbad3d6
Thibaud Duhautbout <thibaud@duhautbout.ovh>
2021-05-20 20:58:18 +0200

    Fix pour le lint

--- flairsou_api/urls.py
+++ flairsou_api/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'flairsou_api'
 urlpatterns = [
-    re_path('accounts/(?P<book>\d+)?$',
+    re_path(r'accounts/(?P<book>\d+)?$',
             views.AccountList.as_view(),
             name="account-list"),
     path('accounts/<int:pk>/',
