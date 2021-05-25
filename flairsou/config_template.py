a6d59e87802cadcc81cdc44812df10a047cbf488
Rémy Huet <pro@remyhuet.fr>
2021-05-25 14:01:55 +0200

    Corrections lint

--- flairsou/config_template.py
+++ flairsou/config_template.py
@@ -1,3 +1,6 @@
+# flake8: noqa
+# (on laisse le commentaire pour la ganération de la clef dépasser)
+
 # Configurations du serveur - template
 # Ce fichier doit être copié sous le nom config.py dans le répertoire flairsou
 # LE FICHIER config.py NE DOIT PAS ETRE COMMIT
@@ -7,7 +10,8 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # clé secrète - à remplacer
-# on peut générer une clé avec la commande suivante (exécutée depuis la racine du projet)
+# on peut générer une clé avec la commande suivante
+# (exécutée depuis la racine du projet) :
 # python manage.py shell -c 'from django.core.management import utils; print(utils.get_random_secret_key())'
 SECRET_KEY = '<SECRET_KEY>'
 
