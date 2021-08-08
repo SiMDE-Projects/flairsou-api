e557f43be0cecda8b736c69ce5c0aaa8cfb2d4d7
Thibaud Duhautbout <thibaud@duhautbout.ovh>
2021-08-08 11:47:24 +0200

    Création d'une superclass sur ModelSerializer pour généraliser les méthodes is_update_request et is_create_request

---
+++ flairsou_api/serializers/flairsou_serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+
+class FlairsouModelSerializer(serializers.ModelSerializer):
+    def is_update_request(self):
+        return self.context['request'].method == 'PUT'
+
+    def is_create_request(self):
+        return self.context['request'].method == 'POST'
+
+    def ValidationError(self, msg):
+        return serializers.ValidationError(msg)
