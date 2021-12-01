139adaeda5935f24992cae915f03e4717e1838e4
Thibaud Duhautbout <thibaud@duhautbout.ovh>
2021-12-01 22:10:48 +0100

    Fix d'une erreur de validation du Book par l'API et ajout des tests manquants

--- flairsou_api/serializers/book_serializer.py
+++ flairsou_api/serializers/book_serializer.py
@@ -11,10 +11,19 @@ class BookSerializer(FlairsouModelSerializer):
 
     def validate_entity(self, entity: uuid.UUID):
         """
+        Vérifie que l'entité n'est pas déjà associée à un livre
         Lors de la mise à jour du champ, vérifie que l'entité associée au
         livre n'est pas modifiée, car les comptes sont attachés aux livres
         """
+        books = Book.objects.all()
+        for book in books:
+            if book.entity == entity:
+                raise self.ValidationError('Un livre est déjà associé '
+                                           'à cette entité')
+
         if self.is_update_request():
             if entity != self.instance.entity:
                 raise self.ValidationError('L\'entité associée au livre '
                                            'ne peut pas être modifiée')
+
+        return entity
