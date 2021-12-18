aee4167414778d122aa61b6721cb63657e4abbad
Thibaud Duhautbout <thibaud@duhautbout.ovh>
2021-12-18 12:24:10 +0100

    Suppression du modèle utilisateur prévu par le mock portail, on va directement utiliser le portail

--- flairsou/settings.py
+++ flairsou/settings.py
@@ -127,8 +127,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'mock_portail.CustomUser'
-
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
