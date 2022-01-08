23cba07b272b505be345d880c85bf157d62db1e4
Thibaud Duhautbout <thibaud@duhautbout.ovh>
2022-01-08 08:25:40 +0100

    Ajout du serializer pour les assos

--- proxy_pda/serializers.py
+++ proxy_pda/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 import uuid
 
+from proxy_pda.models import Asso
+
 
 class UserInfoSerializer(serializers.Serializer):
     """
@@ -19,3 +21,25 @@ AnonymousUserInfo = UserInfoSerializer({
     'lastname': '',
     'id': str(uuid.UUID(int=0))
 })
+
+
+class AssoSerializer(serializers.ModelSerializer):
+    """
+    Serializer permettant de renvoyer la liste des associations auxquelles
+    un utilisateur a accès dans Flairsou
+    """
+    asso_set = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Asso
+        fields = [
+            'asso_id',
+            'shortname',
+            'name',
+            'asso_type',
+            'asso_set',
+        ]
+
+    def get_asso_set(self, instance):
+        sub_assos_granted = instance.get_sub_assos_granted()
+        return AssoSerializer(sub_assos_granted, many=True).data
