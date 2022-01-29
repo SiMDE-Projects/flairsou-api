bbeecd426a6c8698703afb0f2885f073ab8a5c1a
Maël Belval <mael.belval.pro@gmail.com>
2022-01-29 22:39:28 +0100

    Refactor SearchBar to use SCSS and Semantic UI

--- flairsou_frontend/src/components/UI/atoms/SearchBar/SearchBar.jsx
+++ flairsou_frontend/src/components/UI/atoms/SearchBar/SearchBar.jsx
@@ -1,14 +1,8 @@
 import React from 'react';
-import './searchBar.css';
 import { Input } from 'semantic-ui-react';
-import lens from '../../../../assets/lens.svg';
 
 const SearchBar = () => (
-  <div className="search-bar">
-    <img src={lens} alt="lens pictogram" />
-    <Input type="text" placeholder="recherche..." />
-  </div>
-
+  <Input icon="search" iconPosition="left" placeholder="recherche..." fluid />
 );
 
 export default SearchBar;
