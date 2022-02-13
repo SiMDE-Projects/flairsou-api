05cd296498fccf57bd53ced1243ad6b8750dd0c4
Thibaud Duhautbout <thibaud@duhautbout.ovh>
2022-02-13 08:27:29 +0100

    Ajout de documentation sur les composants et les props

--- flairsou_frontend/src/components/UI/atoms/Operation/Operation.jsx
+++ flairsou_frontend/src/components/UI/atoms/Operation/Operation.jsx
@@ -5,6 +5,9 @@ import { Table, Icon } from 'semantic-ui-react';
 import RenderOperation from './RenderOperation/RenderOperation';
 import currencyFormat from '../../../../utils/currencyFormat';
 
+/**
+ * Composant effectuant le rendu d'une transaction dans l'affichage d'un compte
+ */
 const Operation = ({ transaction }) => {
   // indique si la transaction doit être étendue ou non (i.e. si il faut
   // afficher toutes les opérations de la transaction)
@@ -28,6 +31,8 @@ const Operation = ({ transaction }) => {
     const otherOpId = (transaction.currentOpId + 1) % 2;
     otherAccountName = transaction.operations[otherOpId].accountFullName;
   } else {
+    // si la transaction est répartie, on crée directement un composant qui affiche
+    // la transaction répartie et un petit icône qui permet de déplier la transaction
     otherAccountName = (
       <>
         Transaction répartie&nbsp;
@@ -77,19 +82,37 @@ const Operation = ({ transaction }) => {
 };
 
 Operation.propTypes = {
+  // objet transaction à afficher
   transaction: PropTypes.shape({
+    // clé primaire de la transaction dans la base de l'API
     pk: PropTypes.number.isRequired,
+    // date de la transaction
     date: PropTypes.string.isRequired,
+    // indique si la transaction est rapprochée ou non. Une transaction rapprochée passe
+    // en lecture seule
+    is_reconciliated: PropTypes.bool.isRequired,
+    // indique si la transaction est pointée ou non (quand une transaction saisie
+    // et constatée sur le compte en ligne, l'utilisateur peut la pointer pour indiquer
+    // qu'elle est correctement saisie)
     checked: PropTypes.bool.isRequired,
+    // justificatif associé à la transaction (TODO)
     invoice: PropTypes.string,
     balance: PropTypes.number.isRequired,
+    // solde partiel suite à cette transaction
     currentOpId: PropTypes.number.isRequired,
+    // liste des opérations associées à la transaction
     operations: PropTypes.arrayOf(PropTypes.shape({
+      // clé primaire de l'opération dans la base de l'API
       pk: PropTypes.number.isRequired,
+      // montant associé au crédit (centimes)
       credit: PropTypes.number.isRequired,
+      // montant associé au débit (centimes)
       debit: PropTypes.number.isRequired,
+      // label associé à l'opération
       label: PropTypes.string.isRequired,
+      // clé primaire du compte lié à l'opération
       account: PropTypes.number.isRequired,
+      // nom complet du compte lié à l'opération
       accountFullName: PropTypes.string.isRequired,
     })).isRequired,
   }).isRequired,
--- flairsou_frontend/src/components/UI/atoms/Operation/RenderOperation/RenderOperation.jsx
+++ flairsou_frontend/src/components/UI/atoms/Operation/RenderOperation/RenderOperation.jsx
@@ -4,7 +4,11 @@ import { Table } from 'semantic-ui-react';
 
 import currencyFormat from '../../../../../utils/currencyFormat';
 
+/**
+ * Composant effectuant le rendu d'une opération particulière
+ */
 const RenderOperation = ({ operation, accountName, active = false }) => {
+  // affichage du crédit et du débit seulement si le montant est non nul
   const credit = operation.credit > 0 ? currencyFormat(operation.credit) : '';
   const debit = operation.debit > 0 ? currencyFormat(operation.debit) : '';
 
@@ -27,15 +31,30 @@ const RenderOperation = ({ operation, accountName, active = false }) => {
 };
 
 RenderOperation.propTypes = {
+  // opération à afficher
   operation: PropTypes.shape({
+    // clé primaire de l'opération dans la base de l'API
     pk: PropTypes.number.isRequired,
+    // montant associé au crédit (centimes)
     credit: PropTypes.number.isRequired,
+    // montant associé au débit (centimes)
     debit: PropTypes.number.isRequired,
+    // label de l'opération
     label: PropTypes.string.isRequired,
+    // clé primaire du compte lié à l'opération
     account: PropTypes.number.isRequired,
+    // nom complet du compte lié à l'opération
     accountFullName: PropTypes.string.isRequired,
   }).isRequired,
+  // nom de compte à afficher (ce nom ne correspond pas forcément au nom
+  // du compte lié à l'opération : dans le cas d'une transaction simple, on
+  // affiche le nom de l'autre compte pour indiquer vers quel compte se fait le
+  // transfert, et dans le cas d'une transaction répartie, on affiche le nom
+  // "Transaction répartie" sur la première ligne et le nom du compte lié à l'opération
+  // sur les lignes suivantes)
   accountName: PropTypes.string.isRequired,
+  // indique si la ligne correspond à l'opération courante dans la liste des opérations
+  // d'une transaction répartie
   active: PropTypes.bool.isRequired,
 };
 
