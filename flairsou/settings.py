c2255486ca24dbb3e7b6345df833858c74ad34f6
César Richard <cesar.richard@weezevent.com>
2022-03-02 13:26:30 +0100

    Added django BASE_URL env

--- flairsou/settings.py
+++ flairsou/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 
 # import custom config file
 from . import config
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = env('BASE_URL') + "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
