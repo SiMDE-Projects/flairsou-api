aa36706b5134ce2da318fea0a4c1fe6a3cb37ec0
César Richard <cesar.richard@weezevent.com>
2022-06-22 18:16:32 +0200

    Added Sentry to backend and frontend

--- flairsou_frontend/src/index.js
+++ flairsou_frontend/src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import { render } from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import { Provider } from 'react-redux';
-
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import App from './components/App';
 import store from './store';
 
 import './index.css';
+
+Sentry.init({
+  dsn: 'https://8187426c2af7419d95a640b957cbeb9d@o1296214.ingest.sentry.io/6522978',
+  integrations: [new BrowserTracing()],
+  environment: 'developpement',
+  tracesSampleRate: 1.0,
+});
 
 const container = document.getElementById('app');
 
